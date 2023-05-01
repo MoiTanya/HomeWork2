22144adb3609f847a56caf58d0051d8d2940610b
MoiTanya <130609745+MoiTanya@users.noreply.github.com>
2023-05-01 18:40:18 +0300

    init commit task13

---
+++ task13.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+if (Convert.ToInt32(number)<100){
+     Console.WriteLine($"Вы ввели число {number} -> третьей цифры нет");
+     
+}
+else {
+    // int answer = number%10;
+    Console.WriteLine($"{number} -> {number[2]}");
+}
