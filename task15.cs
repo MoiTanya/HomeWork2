8757d39972bf4ec18ef398ece3577fbc143c958b
MoiTanya <130609745+MoiTanya@users.noreply.github.com>
2023-05-01 18:41:05 +0300

    init commit task15

---
+++ task15.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите число, обозначающее неделю");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day<8 && day>0)
+{
+    switch (day){
+        case 1: 
+        Console.WriteLine($"Вы ввели число {day} -> понедельник");
+        break;
+        case 2: 
+        Console.WriteLine($"Вы ввели число {day} -> вторник");
+        break;
+        case 3: 
+        Console.WriteLine($"Вы ввели число {day} -> среда");
+        break;
+        case 4: 
+        Console.WriteLine($"Вы ввели число {day} -> четверг");
+        break;
+        case 5: 
+        Console.WriteLine($"Вы ввели число {day} -> пятница");
+        break;
+        case 6: 
+        Console.WriteLine($"Вы ввели число {day} -> суббота");
+        break;
+        case 7: 
+        Console.WriteLine($"Вы ввели число {day} -> воскресенье");
+        break;
+    }
+     
+}
+else {
+    Console.WriteLine($"{day} -> нет такого дня недели");
+}
